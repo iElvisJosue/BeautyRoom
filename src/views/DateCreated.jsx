78d491a463b5e74480a3bbf2418994ecf791a8cb
Elvis Josue <elvisjosue98@hotmail.com>
2024-04-27 09:54:10 -0600

    Actualización de la vista de la cita creada

--- src/views/DateCreated.jsx
+++ src/views/DateCreated.jsx
@@ -4,24 +4,26 @@ import "../styles/DateCreated.css";
 export default function DateCreated() {
   return (
     <main className="DateCreated">
-      <img
-        src="GraciasPorTuPago.png"
-        alt="Icono De Pago"
-        className="DateCreated__Img"
-      />
-      <p className="DateCreated__Text">
-        ¡Gracias por confiar en nosotros y completar tu pago!
-      </p>
-      <p className="DateCreated__Text Two">
-        Pronto recibirás un mensaje de WhatsApp con los detalles de tu cita.
-        ¡Esperamos ansiosos verte pronto!
-      </p>
-      <a
-        className="DateCreated__Button"
-        href="https://www.embeautyroom.site/AgendarCita"
-      >
-        Agendar otra cita
-      </a>
+      <div className="DateCreated__Container">
+        <img
+          src="GraciasPorTuPago.png"
+          alt="Icono De Pago"
+          className="DateCreated__Container--Img"
+        />
+        <p className="DateCreated__Container--Title">
+          ¡Pago realizado correctamente!
+        </p>
+        <p className="DateCreated__Container--Title Two">
+          Pronto recibirás un mensaje de WhatsApp con los detalles de tu cita.
+          ¡Esperamos ansiosos verte pronto!
+        </p>
+        <a
+          className="DateCreated__Container--Button"
+          href="https://www.embeautyroom.site/AgendarCita"
+        >
+          Agendar otra cita <ion-icon name="arrow-forward-outline"></ion-icon>
+        </a>
+      </div>
     </main>
   );
 }
