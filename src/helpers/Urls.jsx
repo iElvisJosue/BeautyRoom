778b7be119387f67e05dfe5478fc0b063c0f4169
Elvis Josue <elvisjosue98@hotmail.com>
2024-04-30 19:58:18 -0600

    corrigiendo URLS

--- src/helpers/Urls.jsx
+++ src/helpers/Urls.jsx
@@ -1,4 +1,4 @@
-// export const HOST = "https://www.embeautyroom.site";
-// export const HOST_IMG = "https://www.embeautyroom.shop:4100";
-export const HOST = "http://localhost:5173";
-export const HOST_IMG = "http://localhost:4000";
+export const HOST = "https://www.embeautyroom.site";
+export const HOST_IMG = "https://www.embeautyroom.shop:4100";
+// export const HOST = "http://localhost:5173";
+// export const HOST_IMG = "http://localhost:4000";
