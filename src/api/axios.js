fce2b2f171951ac193ac2b0f74eaa28907fff116
Elvis Josue <elvisjosue98@hotmail.com>
2024-05-17 01:18:44 -0600

    Se corrigio error de URLS

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // UTILIZAR CUANDO ESTE EN PRODUCCIÓN
-  baseURL: "https://www.embeautyroom.op:4100/api",
+  baseURL: "https://www.embeautyroom.shop:4100/api",
   // baseURL: "http://localhost:4000/api",
   withCredentials: true,
 });
