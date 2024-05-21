36237c6890b3a473ffe7e0e8302bc5cbc9142c1b
Elvis Josue <elvisjosue98@hotmail.com>
2024-05-21 15:15:34 -0600

    Probando

--- src/helpers/Urls.jsx
+++ src/helpers/Urls.jsx
@@ -1,6 +1,6 @@
-export const HOST = "https://www.embeautyroom.site";
-export const HOST_IMG = "https://www.embeautyroom.shop:4100";
-export const HOST_PDF = "https://www.embeautyroom.shop:4100/PDF";
-// export const HOST = "http://localhost:5173";
-// export const HOST_IMG = "http://localhost:4000";
-// export const HOST_PDF = "http://localhost:4000/PDF";
+// export const HOST = "https://www.embeautyroom.site";
+// export const HOST_IMG = "https://www.embeautyroom.shop:4100";
+// export const HOST_PDF = "https://www.embeautyroom.shop:4100/PDF";
+export const HOST = "http://localhost:5173";
+export const HOST_IMG = "http://localhost:4000";
+export const HOST_PDF = "http://localhost:4000/PDF";
