233c7d17c47ebcbcc61422755b2b24f07afc16ac
Elvis Josue <elvisjosue98@hotmail.com>
2024-06-01 19:49:43 -0600

    Se cambio el diseño de la tabla para las ventas diarias

--- src/components/SalesAll.jsx
+++ src/components/SalesAll.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 
 // IMPORTAMOS LOS COMPONENTES
-import Loader from "../components/Loader";
+// import Loader from "../components/Loader";
 import NotResults from "../components/NotResults";
 
 // IMPORTAMOS LOS HOOKS A USAR
@@ -20,8 +20,8 @@ export default function SalesAll() {
   const [amountPages, setAmountPages] = useState(1);
   const [startIndex, setStartIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(amountSales);
-  const { salesByFilter, searchingSalesByFilter, setFilterSales } =
-    useGetSalesByFilter();
+  // const { salesByFilter, searchingSalesByFilter, setFilterSales } =
+  const { salesByFilter, setFilterSales } = useGetSalesByFilter();
 
   useEffect(() => {
     if (salesByFilter) {
@@ -36,10 +36,9 @@ export default function SalesAll() {
     const regex = /^[a-zA-Z0-9\sáéíóúÁÉÍÓÚüÜ-]*$/;
     // Comprobamos si el nuevo valor cumple con la expresión regular
     if (regex.test(value)) {
-      setStartIndex(0);
-      setEndIndex(amountSales);
       const filter = event.target.value;
       setFilterSales(filter);
+      resetValues();
     }
   };
 
@@ -55,12 +54,18 @@ export default function SalesAll() {
     setPage(page - 1);
   };
 
+  const resetValues = () => {
+    setStartIndex(0);
+    setEndIndex(amountSales);
+    setPage(1);
+  };
+
   return (
     <div className="Sales__All">
       <h1 className="Sales__All--SubTitle">Buscar venta:</h1>
       <input
         type="text"
-        placeholder="Fecha (AAAA-MM-DD), Folio, Producto, Empleado, Metodo de Pago o Cliente"
+        placeholder="Fecha (AAAA-MM-DD), Folio, Producto, Empleado, Cliente"
         className="Sales__All--Input"
         onChange={getSalesByFilter}
       />
@@ -82,72 +87,237 @@ export default function SalesAll() {
           </button>
         )}
       </div>
-      <section className="Sales__All--TableList">
-        <header className="Sales__All--TableList--Header">
-          <p className="Sales__All--TableList--Header--Number">Folio</p>
-          <p className="Sales__All--TableList--Header--Product">Producto</p>
-          <p className="Sales__All--TableList--Header--Total">Total</p>
-          <p className="Sales__All--TableList--Header--Date">Fecha</p>
-          <p className="Sales__All--TableList--Header--Ticket">Ticket</p>
-        </header>
-        {searchingSalesByFilter ? (
-          <Loader />
-        ) : salesByFilter.length > 0 ? (
-          salesByFilter
-            .slice(startIndex, endIndex)
-            .map(
-              (
-                {
-                  Folio,
-                  ImagenProducto,
-                  Cantidad,
-                  Nombre,
-                  TotalProducto,
-                  FechaVenta,
-                  Ticket,
-                },
-                index
-              ) => (
-                <div className="Sales__All--TableList--Details" key={index}>
-                  <p className="Sales__All--TableList--Details--Number">
-                    {Folio}
-                  </p>
-                  <span className="Sales__All--TableList--Details--Product">
-                    <picture className="Sales__All--TableList--Details--Product--Picture">
-                      <img
-                        src={`${HOST_IMG}/${ImagenProducto}`}
-                        alt="Imagen Representativa del Producto"
-                      />
-                    </picture>
-                    <p>
-                      x{Cantidad} - {Nombre}
-                    </p>
-                  </span>
-                  <p className="Sales__All--TableList--Details--Total">
-                    ${TotalProducto.toLocaleString()}
-                  </p>
-                  <p className="Sales__All--TableList--Details--Date">
-                    {FechaVenta.substring(0, 10)}
-                  </p>
-                  <span className="Sales__All--TableList--Details--Ticket">
-                    <a
-                      className="Sales__All--TableList--Details--Ticket--Button"
-                      href={`${HOST_PDF}/${Ticket}`}
-                      target="_blank"
-                    >
-                      <ion-icon name="document-text-outline"></ion-icon>
-                    </a>
-                  </span>
-                </div>
-              )
-            )
-        ) : (
-          <NotResults> No hay ventas disponibles</NotResults>
-        )}
-      </section>
-      <p className="Sales__All--TableList--Pages">
-        ({page}/{amountPages})
-      </p>
+      {salesByFilter.length > 0 ? (
+        <div className="Sales__All--TableList">
+          <table className="Sales__All--TableList--Table">
+            <thead className="Sales__All--TableList--Table--Headers">
+              <tr>
+                <th>Folio</th>
+                <th>Imagen</th>
+                <th>Producto</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Total</th>
+                <th>Empleado</th>
+                <th>Cliente</th>
+                <th>Ticket</th>
+              </tr>
+            </thead>
+            <tbody className="Sales__All--TableList--Table--Body">
+              {salesByFilter
+                .slice(startIndex, endIndex)
+                .map(
+                  (
+                    {
+                      Folio,
+                      ImagenProducto,
+                      Cantidad,
+                      Nombre,
+                      TotalProducto,
+                      FechaVenta,
+                      HoraVenta,
+                      EmpleadoAsignado,
+                      Cliente,
+                      Ticket,
+                    },
+                    index
+                  ) => (
+                    <tr key={index}>
+                      <td>{Folio}</td>
+                      <td>
+                        <img
+                          className="Sales__All--TableList--Table--Body--Picture"
+                          src={`${HOST_IMG}/${ImagenProducto}`}
+                          alt="Imagen Representativa del Producto"
+                        />
+                      </td>
+                      <td>
+                        x{Cantidad} - {Nombre}
+                      </td>
+                      <td>{FechaVenta.substring(0, 10)}</td>
+                      <td>{HoraVenta}</td>
+                      <td>
+                        {TotalProducto.toLocaleString("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                        })}
+                      </td>
+                      <td>{EmpleadoAsignado}</td>
+                      <td>{Cliente}</td>
+                      <td>
+                        <a
+                          className="Sales__All--TableList--Table--Body--Ticket"
+                          href={`${HOST_PDF}/${Ticket}`}
+                          target="_blank"
+                        >
+                          <ion-icon name="document-text-outline"></ion-icon>
+                        </a>
+                      </td>
+                    </tr>
+                  )
+                )}
+            </tbody>
+          </table>
+          <p className="Sales__All--TableList--Pages">
+            ({page}/{amountPages})
+          </p>
+        </div>
+      ) : (
+        <NotResults>¡No se encontraron resultados!</NotResults>
+      )}
     </div>
   );
 }
+// // IMPORTAMOS LAS LIBRERÍAS A USAR
+// import { useState, useEffect } from "react";
+
+// // IMPORTAMOS LOS COMPONENTES
+// import Loader from "../components/Loader";
+// import NotResults from "../components/NotResults";
+
+// // IMPORTAMOS LOS HOOKS A USAR
+// import useGetSalesByFilter from "../hooks/useGetSalesByFilter";
+
+// // IMPORTAMOS LAS AYUDAS
+// import { HOST_IMG, HOST_PDF } from "../helpers/Urls";
+
+// // IMPORTAMOS LOS ESTILOS
+// import "../styles/SalesAll.css";
+
+// export default function SalesAll() {
+//   const amountSales = 25;
+//   const [page, setPage] = useState(1);
+//   const [amountPages, setAmountPages] = useState(1);
+//   const [startIndex, setStartIndex] = useState(0);
+//   const [endIndex, setEndIndex] = useState(amountSales);
+//   const { salesByFilter, searchingSalesByFilter, setFilterSales } =
+//     useGetSalesByFilter();
+
+//   useEffect(() => {
+//     if (salesByFilter) {
+//       const cantidadDePaginas = Math.ceil(salesByFilter.length / amountSales);
+//       setAmountPages(cantidadDePaginas);
+//     }
+//   }, [salesByFilter]);
+
+//   const getSalesByFilter = (event) => {
+//     const value = event.target.value;
+//     // Utilizamos una expresión regular para permitir letras, números y "-"
+//     const regex = /^[a-zA-Z0-9\sáéíóúÁÉÍÓÚüÜ-]*$/;
+//     // Comprobamos si el nuevo valor cumple con la expresión regular
+//     if (regex.test(value)) {
+//       setStartIndex(0);
+//       setEndIndex(amountSales);
+//       const filter = event.target.value;
+//       setFilterSales(filter);
+//     }
+//   };
+
+//   const handleShowTwentyFiveMore = () => {
+//     setStartIndex(startIndex + amountSales);
+//     setEndIndex(endIndex + amountSales);
+//     setPage(page + 1);
+//   };
+
+//   const handleShowTwentyFiveLess = () => {
+//     setStartIndex(startIndex - amountSales);
+//     setEndIndex(endIndex - amountSales);
+//     setPage(page - 1);
+//   };
+
+//   return (
+//     <div className="Sales__All">
+//       <h1 className="Sales__All--SubTitle">Buscar venta:</h1>
+//       <input
+//         type="text"
+//         placeholder="Fecha (AAAA-MM-DD), Folio, Producto, Empleado, Metodo de Pago o Cliente"
+//         className="Sales__All--Input"
+//         onChange={getSalesByFilter}
+//       />
+//       <div className="Sales__All--Buttons--Pages">
+//         {startIndex >= amountSales && (
+//           <button
+//             className="Sales__All--TableList--Buttons--Pages--Button Prev"
+//             onClick={handleShowTwentyFiveLess}
+//           >
+//             <ion-icon name="arrow-back-outline"></ion-icon>
+//           </button>
+//         )}
+//         {endIndex < salesByFilter.length && (
+//           <button
+//             className="Sales__All--TableList--Buttons--Pages--Button Next"
+//             onClick={handleShowTwentyFiveMore}
+//           >
+//             <ion-icon name="arrow-forward-outline"></ion-icon>
+//           </button>
+//         )}
+//       </div>
+//       <section className="Sales__All--TableList">
+//         <header className="Sales__All--TableList--Header">
+//           <p className="Sales__All--TableList--Header--Number">Folio</p>
+//           <p className="Sales__All--TableList--Header--Product">Producto</p>
+//           <p className="Sales__All--TableList--Header--Total">Total</p>
+//           <p className="Sales__All--TableList--Header--Date">Fecha</p>
+//           <p className="Sales__All--TableList--Header--Ticket">Ticket</p>
+//         </header>
+//         {searchingSalesByFilter ? (
+//           <Loader />
+//         ) : salesByFilter.length > 0 ? (
+//           salesByFilter
+//             .slice(startIndex, endIndex)
+//             .map(
+//               (
+//                 {
+//                   Folio,
+//                   ImagenProducto,
+//                   Cantidad,
+//                   Nombre,
+//                   TotalProducto,
+//                   FechaVenta,
+//                   Ticket,
+//                 },
+//                 index
+//               ) => (
+//                 <div className="Sales__All--TableList--Details" key={index}>
+//                   <p className="Sales__All--TableList--Details--Number">
+//                     {Folio}
+//                   </p>
+//                   <span className="Sales__All--TableList--Details--Product">
+//                     <picture className="Sales__All--TableList--Details--Product--Picture">
+//                       <img
+//                         src={`${HOST_IMG}/${ImagenProducto}`}
+//                         alt="Imagen Representativa del Producto"
+//                       />
+//                     </picture>
+//                     <p>
+//                       x{Cantidad} - {Nombre}
+//                     </p>
+//                   </span>
+//                   <p className="Sales__All--TableList--Details--Total">
+//                     ${TotalProducto.toLocaleString()}
+//                   </p>
+//                   <p className="Sales__All--TableList--Details--Date">
+//                     {FechaVenta.substring(0, 10)}
+//                   </p>
+//                   <span className="Sales__All--TableList--Details--Ticket">
+//                     <a
+//                       className="Sales__All--TableList--Details--Ticket--Button"
+//                       href={`${HOST_PDF}/${Ticket}`}
+//                       target="_blank"
+//                     >
+//                       <ion-icon name="document-text-outline"></ion-icon>
+//                     </a>
+//                   </span>
+//                 </div>
+//               )
+//             )
+//         ) : (
+//           <NotResults> No hay ventas disponibles</NotResults>
+//         )}
+//       </section>
+//       <p className="Sales__All--TableList--Pages">
+//         ({page}/{amountPages})
+//       </p>
+//     </div>
+//   );
+// }
